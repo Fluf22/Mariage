bf81f0269c2a93a56cd3461f9dc388803deeb416
Thomas Raffray <thomas.raffray@jenji.io>
2021-04-10 13:10:51 +0200

    Remove unused import

--- src/components/Informations/index.tsx
+++ src/components/Informations/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, useMediaQuery } from "@material-ui/core";
+import { Grid, useMediaQuery } from "@material-ui/core";
 import useStyles from "./styles";
 import Map from "../Map";
 
